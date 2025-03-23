45676bc51246407753cc5b6831d72ea1dde4d89a
Umberto <umbertobianchin.mail@gmail.com>
2025-03-24 00:01:17 +0100

    minor

--- src/main.c
+++ src/main.c
@@ -6,40 +6,33 @@
 #include <utils.h>
 
 int main(int argc, char **argv) {
-	if ( argc < 2 ) {
-		printf("Usage: %s -help for help\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}       
 
-	instance inst;
-
-	initialize_instance(&inst);
-	
-	parse_command_line(argc, argv, &inst);
-	read_input(&inst);
-
-	compute_all_costs(&inst);
-
-	printf("Choose run mode:\n");
-    printf(" - 'b' for benchmark\n");
+    printf("Choose run mode:\n");
+    printf(" - 'b' for benchmark by params\n");
     printf(" - 'n' for normal execution\n");
 
     char mode = tolower(getchar());
     getchar();
 
-	if (mode == 'b') {
+    if (mode == 'b') {
         printf("Running in BENCHMARK mode.\n\n");
-        benchmark_algorithm_by_time(&inst);
+        benchmark_algorithm_by_params();
     } else if (mode == 'n') {
         printf("Running in NORMAL mode.\n\n");
+        instance inst;
+
+	    initialize_instance(&inst);
+
+	    parse_command_line(argc, argv, &inst);
+	    read_input(&inst);
+
+	    compute_all_costs(&inst);
         choose_run_algorithm(&inst);
+        free_instance(&inst);
     } else {
         printf("Unknown mode '%c'. Exiting.\n", mode);
-        free_instance(&inst);
         exit(EXIT_FAILURE);
     }
-	
-	free_instance(&inst);
 	
 	return 0;
 }
