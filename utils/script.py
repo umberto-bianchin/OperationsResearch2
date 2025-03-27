c23ad0d01cbb1e8a03ef85bba1542b2a51aee21c
FrafUni <300832@studenti.unimore.it>
2025-03-27 23:37:26 +0100

    script

--- utils/script.py
+++ utils/script.py
@@ -1,15 +1,35 @@
 import subprocess
 
 commands = [
-    "./tsp -n 1000 -t 60 -r b -a v -kick 5",
+    "./tsp -n 1000 -t 60 -r b -a v -kick 1 -kopt 3",
     "sleep 2",
-    "./tsp -n 1000 -t 60 -r b -a v -kick 10",
+    "./tsp -n 1000 -t 60 -r b -a v -kick 5 -kopt 3",
     "sleep 2",
-    "./tsp -n 1000 -t 60 -r b -a t -maxt 500 -mint 100 -stept 50",
+    "./tsp -n 1000 -t 60 -r b -a v -kick 1 -kopt 5",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a v -kick 3 -kopt 5",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a v -kick 1 -kopt 7",
     "sleep 2",
     "./tsp -n 1000 -t 60 -r b -a t -maxt 50 -mint 10 -stept 5",
     "sleep 2",
-    "./tsp -n 1000 -t 60 -r b -a t -maxt 5000 -mint 1000 -stept 100"
+    "./tsp -n 1000 -t 60 -r b -a t -maxt 155 -mint 25 -stept 10",     
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a t -maxt 250 -mint 50 -stept 25",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a t -maxt 500 -mint 100 -stept 50",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a t -maxt 155 -mint 25 -stept 10",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a g -alpha 20 -minc 3",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a g -alpha 50 -minc 3",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a g -alpha 100 -minc 3",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a g -alpha 20 -minc 7",
+    "sleep 2",
+    "./tsp -n 1000 -t 60 -r b -a g -alpha 50 -minc 7"    
 ]
 
 for command in commands:
