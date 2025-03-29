9968b7af8d75020c58b729019e60d5c877bca485
FrafUni <300832@studenti.unimore.it>
2025-03-29 12:51:33 +0100

    minors

--- include/csv_parser.h
+++ include/csv_parser.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ROWS 11          //NUM_FILES + 1 (header)
-#define MAX_COLS 50             //maximum number of columns in the file
+#define MAX_ROWS 11                 //NUM_FILES + 1 (header)
+#define MAX_COLS 50                 //maximum number of columns in the file
 #define MAX_LINE_LEN 1024
 #define FILENAME "results/results"
-#define MULTIPLE_FILES 1     //true: results are separeted in file for each algorithm
+#define MULTIPLE_FILES 1            //1: results are separeted in file for each algorithm
 
 int parse_csv(char ***csvData, char *fileName);
 void write_csv(double bestCosts[MAX_ROWS - 1], char *algorithmID, char alg);
